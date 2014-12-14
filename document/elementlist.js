f7c892c4c5a0a2aaaa74566b400efa1b0ff9255f
stevleibelt <stev@leibelt.de>
2014-12-14 21:26:12 +0100

    added api

---
+++ document/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Exception"],["c","Net\\Bazzline\\Component\\Command\\Command"],["c","Net\\Bazzline\\Component\\Command\\RuntimeException"],["c","RuntimeException"]];
---
+++ generate_api.php
@@ -0,0 +1,3 @@
+#!/bin/php
+<?php
+system('php vendor/bin/apigen --source source/Net/Bazzline/Component/Command/ --destination document/ --title "Command by Bazzline"');
